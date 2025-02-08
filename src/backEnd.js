a338784df6e4fa3a4a44f4e4d617b24f253c0ffa
Mars180 <161188335+Mars180@users.noreply.github.com>
2025-02-08 15:12:36 -0600

    Updated

---
+++ src/backEnd.js
@@ -0,0 +1,8 @@
+// fake data set for demo
+let suppliers = new Map([
+    ["supplierOne", ["Wholesale", "5", "Shellfish", "Crustacean"]],
+    ["supplierTwo", ["Wholesale", "5", "Shellfish", "Crustacean"]],
+    ["supplierThree", ["Wholesale", "30", "Shellfish", "Mullosk"]],
+    ["supplierFour", ["Market", "10", "Fish", "Crustacean"]],
+    ["supplierFive", ["Market", "15", "Fish", "Mullosk"]]
+]);
