6df496d52d53d4b100a002ab44388b711d7e1a5f
Mars180 <161188335+Mars180@users.noreply.github.com>
2025-02-08 16:14:22 -0600

    Updated

--- src/backEnd.js
+++ src/backEnd.js
@@ -6,3 +6,35 @@ let suppliers = new Map([
     ["supplierFour", ["Market", "10", "Fish", "Crustacean"]],
     ["supplierFive", ["Market", "15", "Fish", "Mullosk"]]
 ]);
+
+let userList = ["Wholesale", "5", "Shellfish", "Crustacean"];
+
+function bizInfo (value) {
+   //userList.push(value); 
+   console.log(value); 
+}
+
+let valid = 0;
+let validList = [];
+if (userList.length == 4) {
+    for (let i = 0; i < suppliers.length; i++) {
+        if (valid == 4) {
+            validList.push(suppliers[i])
+        }
+        else {
+            for (let j = 0; j < suppliers[i].length; j++) {
+                if (suppliers[i][j] != userList[j]) {
+                    break;
+                    valid = 0;
+                }
+                else {
+                    valid += 1;
+                }
+            }
+        }
+    }
+}
+
+for (let i = 0; i < validList.length; i++) {
+    console.log(validList[i]);
+}
